15269c601be7c0552d5039c6dafd39e87932abeb
234mph <148212310+234mph@users.noreply.github.com>
2024-01-21 00:29:42 +0600

    Delete Task3 directory

--- Task3/RunFactory.java
+++
@@ -1,8 +0,0 @@
-package Task3;
-
-public class RunFactory {
-    public static void main(String[] args) {
-        Manager man = new Manager(35, "Vasya", "male", 120);
-        man.Sold1();
-    }
-}
